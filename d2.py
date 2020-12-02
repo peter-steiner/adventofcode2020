870fe9e459fce52293c49c500dab86da30865107
Peter Steiner <peter.steiner.79.ps@gmail.com>
2020-12-02 05:13:49 +0100

    D2 prep

---
+++ d2.py
@@ -0,0 +1,39 @@
+#!/user/bin/env python3 -tt
+"""
+Task:
+https://adventofcode.com/2020/day/2
+"""
+
+# Imports
+import sys
+import os
+import re
+import math
+
+# Global variables
+task="d2-t"
+infile=task + ".input"
+
+def readInput():
+    with open('input/' + infile) as file:
+        data = file.read()
+    file.close()
+    return data
+
+def a():
+    items = [int(n) for n in readInput().split('\n')]
+    res = 0
+
+    print("A): ", res)
+
+def b():
+    items = [int(n) for n in readInput().split('\n')]
+    res = 0
+
+    print("B): ", res)
+
+# Main body
+if __name__ == '__main__':
+    a()
+    b()
+    sys.exit(1)
