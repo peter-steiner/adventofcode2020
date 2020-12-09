df84c9b87419dc1d631f38bce918cca85ce210aa
Peter Steiner <peter.steiner.79.ps@gmail.com>
2020-12-09 12:31:42 +0100

    D9B

--- d9.py
+++ d9.py
@@ -45,7 +45,47 @@ def a():
         pos += 1
     print("A): ", res)
 
+def b():
+    nmbrs = [int(n) for n in readInput().split('\n')]
+    res = 0
+
+    preambleLength = 25
+    preamble = []
+    pos = preambleLength
+    invalidNmbr = 0
+
+    while pos < len(nmbrs):   
+        preamble = nmbrs[pos-preambleLength:pos]     
+        result = list(itertools.combinations(preamble, 2))
+        match = False
+        for i in result:
+            if sum(i) == nmbrs[pos]:
+                match = True
+        if not match:
+            invalidNmbr = nmbrs[pos] 
+
+        pos += 1
+
+    match = False
+    for i in range(len(nmbrs)):
+        testSUm = 0
+        pos = i
+        nmbrRange = []
+        while testSUm <= invalidNmbr and pos < len(nmbrs)-1:
+            nextNmbr = nmbrs[pos]
+            nmbrRange.append(nextNmbr)
+            if sum(nmbrRange) == invalidNmbr:
+                res = min(nmbrRange) + max(nmbrRange)
+                match = True
+                break
+            pos += 1
+        if match:
+            break
+    print("B): ", res)
+
+
 # Main body
 if __name__ == '__main__':
     a()
+    b()
     sys.exit(1)
