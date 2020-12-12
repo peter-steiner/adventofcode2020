17ed9ef05a5db41056b3871c4a6964586174d802
Peter Steiner <peter.steiner.79.ps@gmail.com>
2020-12-12 14:05:09 +0100

    D12 B

--- d12-B.py
+++ d12-B.py
@@ -21,79 +21,64 @@ def readInput():
     return data
 
 degrees = 0
-def moveShip(instruction, x, y, degrees):
+def moveShip(instruction, x, y, sx, sy):
     comp = re.compile("([a-zA-Z]+)([0-9]+)") 
     action, value = comp.match(instruction).groups() 
     value = int(value)
-    #print("B", action, value, x, y, degrees)
     
-    prevD = degrees
     if action == "R":
-        degrees += value
-        if degrees >= 360:
-            degrees = degrees - 360
-        print("-------------------------------")
-        print("TR: {} \tB {} \tA {}".format(instruction, prevD, degrees))
+        if value >= 180:
+            x = -x
+            y = -y
+            #Handle 270 (290-180 = 90)
+            value -= 180
+        if value == 90:
+            # [1, -1]
+            nv = [y, -x]
+            x = nv[0]
+            y = nv[1]
 
     if action == "L":
-        degrees -= value
-        if degrees < 0:
-            degrees = 360 - abs(degrees)
-        print("-------------------------------")
-        print("TL: {} \tB {} \tA {}".format(instruction, prevD, degrees))
-    
+        if value >= 180:
+            x = -x
+            y = -y
+            #Handle 270 (290-180 = 90)
+            value -= 180
+        if value == 90:
+            # [1, -1]
+            nv = [-y, x]
+            x = nv[0]
+            y = nv[1]
+
     if action == "E":
         x += value
     if action == "W":
         x -= value
     if action == "S":
-        y += value
-    if action == "N":
         y -= value
-#--
-    if degrees%180 == 0:
-        if action == "F":
-            if  degrees == 0:
-                x += value
-            else: 
-                x -= value
-                degrees = 180    
-    # |
-    # |        
-    else:         
-        if action == "F":
-            if  degrees == 270:
-                y -= value
-            else: 
-                y += value
-        
-    return x, y, degrees
+    if action == "N":
+        y += value
+
+    if action == "F":
+        sx += value * x
+        sy += value * y 
+
+    return x, y, sx, sy
 
 def a():
     instructions = [n for n in readInput().split('\n')]
-    x = 0
-    y = 0
-    degrees = 0
-    """
+    x = 10
+    y = 1
+    sx = 0
+    sy = 0
+
     for instr in instructions:
-        print(instr)
-        x, y, degrees = moveShip(instr, x, y, degrees)
-    """
-    for i in range(0, len(instructions)):
-        #print(instructions[i])
-        x, y, degrees = moveShip(instructions[i], x, y, degrees)
+        x, y, sx, sy = moveShip(instr, x, y, sx, sy)
         #print("-------------------------------")
 
-    print("A): ", abs(x)+abs(y))
-
-def b():
-    rows = [int(n) for n in readInput().split('\n')]
-    res = 0
-
-    print("B): ", res)
+    print("A): ", abs(sx)+abs(sy))
 
 # Main body
 if __name__ == '__main__':
     a()
-#    b()
     sys.exit(1)
