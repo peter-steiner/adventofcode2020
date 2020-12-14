3c72295b3adc0b11f3b8dad7dd3aa5aa8b3df16a
Peter Steiner <peter.steiner.79.ps@gmail.com>
2020-12-14 18:34:21 +0100

    D14 A

--- d14.py
+++ d14.py
@@ -11,7 +11,7 @@ import re
 import math
 
 # Global variables
-task="d14-t"
+task="d14"
 infile=task + ".input"
 
 def readInput():
@@ -20,11 +20,77 @@ def readInput():
     file.close()
     return data
 
-def a():
-    rows = [int(n) for n in readInput().split('\n')]
-    res = 0
+mask = list(range(36))
 
-    print("A): ", res)
+class Mem():
+    def __init__(self, id):
+        self.id = id
+        self.mem = format(0, '036b')
+    def mask(self, mask):
+        tmpMem = list(self.mem)
+        for m in range(len(mask)):
+            if mask[m] != "X":
+                tmpMem[m] = mask[m]
+        self.mem = "".join(tmpMem)
+
+    def getIntValue(self):
+        return int(self.mem, 2)
+
+    def addInt(self, val):
+#        self.mem = format(int(self.mem, 2) + val, '036b')
+        self.mem = format(val, '036b')
+    
+    def p(self):
+        print(self.mem, self.getIntValue())
+
+def a():
+    rows = [n for n in readInput().split('\n')]
+
+    global mask
+    maxMem = 0
+    #Init bank
+    for r in rows:
+        if "mem" in (r):
+            mem, val = r.split(" = ")
+            mem = re.findall('\d+', mem)[0]
+            mem = int(mem)
+            if mem > maxMem: 
+                maxMem = mem
+
+    bank = [-1] * (maxMem+1)
+    print(len(mask), maxMem)
+    for r in rows:
+        if "mask" in (r):
+            mask = list(r.split(" = ")[1])
+            print(r)
+        else: 
+            mem, val = r.split(" = ")
+            mem = re.findall('\d+', mem)[0]
+            val = int(val)
+            mem = int(mem)
+
+            if bank[mem] != -1:
+                m = bank[mem]
+                m.addInt(val)
+                m.mask(mask)
+            else: 
+                print("Create for ", mem)
+                m = Mem(mem)
+                m.addInt(val)
+                m.mask(mask)
+                bank[mem] = m
+            bank[mem].p()
+
+    print("--------------------")
+
+    sum = 0
+    for b in range(len(bank)):
+        if bank[b] != -1:
+            #bank[b].p()
+            sum += bank[b].getIntValue()
+
+    print("A): ", sum)
+
 
 def b():
     rows = [int(n) for n in readInput().split('\n')]
