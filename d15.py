ee3a236b7591b78ad6e25660945acfaae493eb47
Peter Steiner <peter.steiner.79.ps@gmail.com>
2020-12-15 12:12:57 +0100

    D15 A

--- d15.py
+++ d15.py
@@ -23,7 +23,28 @@ def readInput():
 
 def a():
     rows = [n for n in readInput().split('\n')]
-    print("B): ", rows)
+    numb = rows[0].split(",")
+    
+    #numb = "1,3,2".split(",")
+    print(numb)
+    numbers = ["X"]*2021
+    for n in range(len(numb)):
+        numbers[n] = int(numb[n])
+    print("A): ", rows)
+    turn = 0
+    while turn < 2020:
+        if numbers[turn] == "X":
+            spoken = numbers[turn-1]
+            if numbers.count(spoken) > 1: 
+                indices = sorted([i for i, x in enumerate(numbers) if x == spoken], reverse=True)
+                last, prev = indices[0:2]
+                speak = last - prev
+                numbers[turn] = speak
+                #break
+            else:
+                numbers[turn] = 0
+        turn += 1
+    print(numbers[2019])
 
 def b():
     rows = [n for n in readInput().split('\n')]
